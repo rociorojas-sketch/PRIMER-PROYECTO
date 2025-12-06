d6fa0738d6c12db78c6fd211cb9db169c8caae15
Rocio Rojas <rocio.rojas@cun.edu.co>
2025-12-06 17:47:57 -0500

    Primer commit: Agregar README y programa hola_mundo

---
+++ README.txt
@@ -0,0 +1,2 @@
+echo # Mi Primer Proyecto > README.txt
+echo Este es un proyecto de ejemplo para aprender Git >> README.txt
---
+++ hola_mundo.py
@@ -0,0 +1,6 @@
+# Programa: Hola Mundo 
+# Autor: Tu Nombre 
+# Fecha: Diciembre 2025 def saludar():
+ print("¡Hola, mundo desde Git!") 
+ print("Este es mi primer proyecto versionado") 
+ if __name__ == "__main__": saludar()
